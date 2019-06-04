25f0bf8516ebb641866dea96dc95cde0c99f7ba6
Cheukting <cheukting.ho@gmail.com>
2019-06-04 15:42:03 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Cheukting/picknmix',
-    version='0.1.0',
+    version='0.1.1',
     python_requires=">=3.5",
     zip_safe=False,
 )
