e8291385cd64ffea97fd9e2e8e42c242da6bc7c9
mani <sadhak001@gmail.com>
2019-09-17 20:25:41 +0100

    Change version of pluggy to 0.13.0, change the runonCICD.sh script to use make coverage instead of multiple commands to execute coverage

--- runOnCICD.sh
+++ runOnCICD.sh
@@ -8,7 +8,4 @@ echo "Running tox..."
 tox
 
 echo "Running coverage..."
-pip install -r requirements-coverage.txt
-coverage run --source picknmix -m pytest
-coverage report -m
-coverage html
+make coverage
