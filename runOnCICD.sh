544c27f15e71cda575ac8fa5dee2845f735879af
mani <sadhak001@gmail.com>
2019-10-02 23:59:01 +0100

    Move the logic of checking for code-related file changes in PR/Branch into the .travis.yml file from the runOnCICD.sh script

--- runOnCICD.sh
+++ runOnCICD.sh
@@ -4,11 +4,6 @@ set -e
 set -u
 set -o pipefail
 
-if [[ "$(./codeChangesAreMade.sh)" = "false" ]]; then
-   echo "We have not made code related changes in this PR/branch, hence exiting gracefully now. No TravisCI build will be triggered for this PR/branch."
-   exit 0
-fi
-
 echo "Running tox..."
 tox
 
