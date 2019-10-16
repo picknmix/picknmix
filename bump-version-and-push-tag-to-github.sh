6c260ffdad01a0eae79de62f7c46e5c305c15ba7
mani <sadhak001@gmail.com>
2019-10-16 23:09:10 +0100

    Updating bump script

--- bump-version-and-push-tag-to-github.sh
+++ bump-version-and-push-tag-to-github.sh
@@ -5,7 +5,7 @@ set -u
 set -o pipefail
 
 echo "Bumping the version of package to next version"
-bumpversion setup.py
+bumpversion --list patch setup.py
 
 echo "Tag created $(git tag --list | head -n 1)"
 
