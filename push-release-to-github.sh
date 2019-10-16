d613beae009d6f348dabc4b1198fc65d6b5ec883
mani <sadhak001@gmail.com>
2019-10-16 21:23:56 +0100

    Script to push current release of source code to GitHub releases after tagging a release (fixes #66)

---
+++ push-release-to-github.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+TARGET_REPO="picknmix/picknmix"
+
+if [[ -z "${GITHUB_TOKEN}" ]]; then
+  echo "GITHUB_TOKEN cannot be found in the current environment, please populate to proceed."
+  exit -1
+fi
+
+TAG_NAME="${1:-}"
+if [[ -z "${TAG_NAME}" ]]; then
+  echo ""
+  echo "Tag name is not specified, please pass it as a CLI arg"
+  echo "   $0 [tag name]"
+  echo "   for e.g. $0 v0.1"
+  echo ""
+
+  echo "Use git tag --list to find out available tags (tag names)"
+  exit -1
+fi
+
+POST_DATA=$(printf '{
+  "tag_name": "%s",
+  "target_commitish": "master",
+  "name": "%s",
+  "body": "Release %s",
+  "draft": false,
+  "prerelease": false
+}' ${TAG_NAME} ${TAG_NAME} ${TAG_NAME})
+echo "Creating release ${RELEASE_VERSION}: $POST_DATA"
+curl \
+    -H "Authorization: token ${GITHUB_TOKEN}" \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/vnd.github.v3+json" \
+    -X POST -d "${POST_DATA}" "https://api.github.com/repos/${TARGET_REPO}/releases"
+
+mkdir -p artifacts
+CURL_OUTPUT="./artifacts/github-release.listing"
+echo "Getting Github ReleaseId"
+curl \
+    -H "Authorization: token ${GITHUB_TOKEN}" \
+    -H "Accept: application/vnd.github.v3+json" \
+    -X GET "https://api.github.com/repos/${TARGET_REPO}/releases/tags/${TAG_NAME}" |
+    tee ${CURL_OUTPUT}
+RELEASE_ID=$(cat ${CURL_OUTPUT} | grep id | head -n 1 | tr -d " " | tr "," ":" | cut -d ":" -f 2)
+echo "Release has been created ${RELEASE_ID} for tag ${TAG_NAME}"
+
+echo ""
+echo "Finished uploading to GitHub"
+echo ""
+echo "Checkout curl output at ${CURL_OUTPUT}"
+echo ""
+echo "Use curl -O -L [github release url] to download an installable artifact."
+echo "    for e.g."
+echo "        curl -O -L https://github.com/picknmix/releases/download/v0.1.tgz"
+echo "    tar xvcf  v0.1.tgz"
+echo "    pip install v0.1"
