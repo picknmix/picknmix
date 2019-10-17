133e58e3f26d838821d71b9e98b7415d425603c8
mani <sadhak001@gmail.com>
2019-10-17 01:42:23 +0100

    Adding extra info at the end of tag creation, on how to delete it

--- bump-version-and-push-tag-to-github.sh
+++ bump-version-and-push-tag-to-github.sh
@@ -11,7 +11,6 @@ if [[ -z "${EXISTING_TAGS}" ]]; then
 else
 	echo "List of tags on local repo (in sync with the remote repo):"
 	echo ${EXISTING_TAGS}
-	echo ""
 fi
 
 echo "Current version of the library is $(grep "version" setup.py | tr -d " ," || true)"
@@ -19,16 +18,21 @@ echo "Current version of the library is $(grep "version" setup.py | tr -d " ," |
 read -p "Do you want to bump the version of your library to the next version ([Y|y]es/[N|n]o)? " answer
 answer="$(echo "${answer}" | awk '{print tolower($0)}')"
 if [[ "${answer}" = "y" ]] || [[ "${answer}" = "yes" ]]; then
-	echo "Bumping the version of library to next version"
+	echo ""; echo "Bumping the version of library to next version"
 	bumpversion --verbose --list patch setup.py
+   
+	echo ""; echo "New version of the library is $(grep "version" setup.py | tr -d " ," || true)"
 
-	echo "New version of the library is $(grep "version" setup.py | tr -d " ," || true)"
 	CREATED_TAG_NAME="$(git tag --list | head -n 1)"
-	echo "Tag created ${CREATED_TAG_NAME}"
-	echo "Recent commit: $(git log --oneline | head -n 1 || true)"
+	echo ""; echo "Tag created ${CREATED_TAG_NAME}"
 
-	echo "Pushing ${CREATED_TAG_NAME} tag to remote"
+	echo ""; echo "Recent commit: $(git log --oneline | head -n 1 || true)"
+
+	echo ""; echo "Pushing ${CREATED_TAG_NAME} tag to remote"
 	git push origin ${CREATED_TAG_NAME}	
+
+	echo ""; echo "You can delete the ${CREATED_TAG_NAME} tag from both your local and remote repos using the below command:"
+	echo "./delete-tag.sh ${CREATED_TAG_NAME}"
 else
-	echo "You said 'no', and hence NOT bumping version of library (version stays unchanged)."
+	echo ""; echo "You said 'no', and hence NOT bumping version of library (version stays unchanged)."
 fi
