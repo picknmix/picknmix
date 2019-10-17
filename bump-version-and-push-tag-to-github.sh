d5b19805c52a1441ca692af16b3169984f9edaa8
mani <sadhak001@gmail.com>
2019-10-17 01:37:35 +0100

    Making bump version script lot more user friendly and printing human readable messages

--- bump-version-and-push-tag-to-github.sh
+++ bump-version-and-push-tag-to-github.sh
@@ -4,10 +4,31 @@ set -e
 set -u
 set -o pipefail
 
-echo "Bumping the version of package to next version"
-bumpversion --list patch setup.py
+EXISTING_TAGS="$(git tag --list || true)"
+echo "~~~ For your information"
+if [[ -z "${EXISTING_TAGS}" ]]; then
+	echo "We found no tags on your local repo (which means we have none on your remote repo either)"
+else
+	echo "List of tags on local repo (in sync with the remote repo):"
+	echo ${EXISTING_TAGS}
+	echo ""
+fi
 
-echo "Tag created $(git tag --list | head -n 1)"
+echo "Current version of the library is $(grep "version" setup.py | tr -d " ," || true)"
 
-echo "Pushing created tag to remote"
-git push --tags
+read -p "Do you want to bump the version of your library to the next version ([Y|y]es/[N|n]o)? " answer
+answer="$(echo "${answer}" | awk '{print tolower($0)}')"
+if [[ "${answer}" = "y" ]] || [[ "${answer}" = "yes" ]]; then
+	echo "Bumping the version of library to next version"
+	bumpversion --verbose --list patch setup.py
+
+	echo "New version of the library is $(grep "version" setup.py | tr -d " ," || true)"
+	CREATED_TAG_NAME="$(git tag --list | head -n 1)"
+	echo "Tag created ${CREATED_TAG_NAME}"
+	echo "Recent commit: $(git log --oneline | head -n 1 || true)"
+
+	echo "Pushing ${CREATED_TAG_NAME} tag to remote"
+	git push origin ${CREATED_TAG_NAME}	
+else
+	echo "You said 'no', and hence NOT bumping version of library (version stays unchanged)."
+fi
