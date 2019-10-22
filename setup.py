f4577e4eb6d9db62897f2247774c27ab95574484
James Myatt <james@jamesmyatt.co.uk>
2019-10-22 23:45:31 +0100

    Numpy is required to import `picknmix`

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = []
+requirements = ["numpy"]
 extras_requirements = {'test': ['pytest', 'scikit-learn>=0.21.2']}
 
 setup(
